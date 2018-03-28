624c77afcc97ff5c24712628afb236fd8fa893ae
apoc64 <sschwedt@gmail.com>
2018-03-28 10:09:40 -0600

    exhibits have names, costs, and patrons

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,7 +1,13 @@
 class Exhibit
+  attr_reader :name,
+              :cost
 
-  def initialize(name, cost)
+  attr_accessor :patrons
 
-  end 
+  def initialize(name, cost = 0)
+    @name = name
+    @cost = cost
+    @patrons = []
+  end
 
 end
--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -12,6 +12,23 @@ class ExhibitTest < MiniTest::Test
     assert_instance_of Exhibit, ex
   end
 
+  def test_it_has_a_name_and_cost
+    ex = Exhibit.new("gems", 10)
+    assert_equal "gems", ex.name
+    assert_equal 10, ex.cost
+  end
+
+  def tests_it_can_have_patrons
+    ex = Exhibit.new("gems", 10)
+    bob = Patron.new("Bob")
+    sally = Patron.new("Sally")
+    ex.patrons << bob
+    ex.patrons << sally
+    assert_equal [bob, sally], ex.patrons
+    assert_equal 2, ex.patrons.count
+    assert_equal "Bob", ex.patrons[0].name
+  end
+
   # Iteration 3 - Exhibit Tracking
   # Add the following methods to your Museum class:
   #
